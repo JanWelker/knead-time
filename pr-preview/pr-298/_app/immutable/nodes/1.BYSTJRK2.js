69bcbbe7dde86b36b18f78d81e7b995e5d5b2e8b
JanWelker <JanWelker@users.noreply.github.com>
2026-09-07 11:54:21 +0000

    Deploy preview for PR 298 🛫

---
+++ pr-preview/pr-298/_app/immutable/nodes/1.BYSTJRK2.js
@@ -0,0 +1,1 @@
+import{O as e,W as t,X as n,Y as r,Z as i,at as a,it as o,j as s,k as c}from"../chunks/B3BZCmpK.js";import"../chunks/xihTtKlq.js";import{t as l}from"../chunks/DkypR3_b.js";var u=s(`<h1> </h1> <p> </p>`,1);function d(s,d){a(d,!0);var f=u(),p=r(f),m=n(p,!0),h=i(p,2),g=n(h,!0);t(()=>{e(m,l.status),e(g,l.error?.message)}),c(s,f),o()}export{d as component};
